2d34428ad30c228516b9a860d0c6e1adc15c49af
Moises Velasquez <mvelasquez@pictoric.com>
2022-06-09 23:40:23 -0500

    preparing project

--- src/app/components/reactive/reactive.module.ts
+++ src/app/components/reactive/reactive.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveContainerComponent } from './reactive-container/reactive-container.component';
 
-
-
 @NgModule({
-  declarations: [
-    ReactiveContainerComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ReactiveContainerComponent],
+  imports: [CommonModule],
+  exports: [ReactiveContainerComponent],
 })
-export class ReactiveModule { }
+export class ReactiveModule {}
