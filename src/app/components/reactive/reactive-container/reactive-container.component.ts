7b9b4d031099523c0b932391e3d7aa284b969f4c
Moises Velasquez <mvelasquez@pictoric.com>
2022-06-10 09:05:52 -0500

    working with subscribe legacy

--- src/app/components/reactive/reactive-container/reactive-container.component.ts
+++ src/app/components/reactive/reactive-container/reactive-container.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-reactive-container',
   templateUrl: './reactive-container.component.html',
-  styleUrls: ['./reactive-container.component.scss']
+  styleUrls: ['./reactive-container.component.scss'],
 })
 export class ReactiveContainerComponent implements OnInit {
+  constructor() {
+    let number = 0;
 
-  constructor() { }
+    const myObservable = new Observable<number>((observer) => {
+      setInterval(() => {
+        number++;
+        observer.next(number);
+        observer.complete();
 
-  ngOnInit(): void {
+        if (number == 3) {
+          observer.error('Wrong number');
+        }
+      }, 3000);
+    });
+
+    myObservable.subscribe(
+      (result) => {
+        console.log(result);
+      },
+      (error) => {
+        console.error(error);
+      },
+      () => {
+        console.log('Observable completed');
+      }
+    );
   }
 
+  ngOnInit(): void {}
 }
