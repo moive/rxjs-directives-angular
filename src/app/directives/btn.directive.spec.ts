f37c10a1c09e9155cf150ceda5c01740533594b0
Moises Velasquez <mvelasquez@pictoric.com>
2022-06-14 18:52:51 -0500

    testing btn button

--- src/app/directives/btn.directive.spec.ts
+++ src/app/directives/btn.directive.spec.ts
@@ -1,8 +1,35 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { AppComponent } from '../app.component';
 import { BtnDirective } from './btn.directive';
 
 describe('BtnDirective', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(async () => {
+    fixture = await TestBed.configureTestingModule({
+      declarations: [AppComponent, BtnDirective],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
   it('should create an instance', () => {
-    // const directive = new BtnDirective();
-    // expect(directive).toBeTruthy();
+    expect(component).toBeTruthy();
+  });
+
+  it('Should create a button with purple background, text color white and width 100%', () => {
+    const element: HTMLButtonElement = fixture.debugElement.query(
+      By.directive(BtnDirective)
+    ).nativeElement;
+
+    expect(element.style.backgroundColor).toEqual('purple');
+    expect(element.style.color).toEqual('white');
+    expect(element.style.width).toEqual('100%');
   });
 });
