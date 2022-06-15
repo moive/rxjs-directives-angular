63fd50c1939bbe5e3f601e2855d03e83fd418ed1
Moises Velasquez <mvelasquez@pictoric.com>
2022-06-14 22:26:38 -0500

    testing area-mouse directive

--- src/app/components/reactive/directives/area-mouse.directive.spec.ts
+++ src/app/components/reactive/directives/area-mouse.directive.spec.ts
@@ -1,8 +1,34 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { ReactiveContainerComponent } from '../reactive-container/reactive-container.component';
 import { AreaMouseDirective } from './area-mouse.directive';
 
 describe('AreaMouseDirective', () => {
+  let component: ReactiveContainerComponent;
+  let fixture: ComponentFixture<ReactiveContainerComponent>;
+
+  beforeEach(async () => {
+    fixture = await TestBed.configureTestingModule({
+      declarations: [ReactiveContainerComponent, AreaMouseDirective],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ReactiveContainerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
   it('should create an instance', () => {
-    // const directive = new AreaMouseDirective();
-    // expect(directive).toBeTruthy();
+    expect(component).toBeTruthy();
+  });
+
+  it('Should create a button with orange background and height 400px', () => {
+    const element: HTMLDivElement = fixture.debugElement.query(
+      By.directive(AreaMouseDirective)
+    ).nativeElement;
+
+    expect(element.style.backgroundColor).toEqual('orange');
+    expect(element.style.height).toEqual('400px');
   });
 });
